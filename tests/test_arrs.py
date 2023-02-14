6a04e70aac5e54612546a465f87f585605ca276b
kseniyaav <kseniyaav10@gmail.com>
2023-02-14 20:55:54 +0300

    -tests added, coverage improved

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -12,7 +12,5 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([], 1) == []
-    #assert arrs.my_slice([1, 2, 3, 4], -6) == [1,2, 3, 4]
-    #assert arrs.my_slice([1, 2, 3, 4], -11) == [1, 2, 3, 4]
     assert arrs.my_slice([1, 2, 3, 4], -2) == [3, 4]
     assert arrs.my_slice([1, 2, 3, 4], 11) == []
